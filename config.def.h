0508674019c6fa2cc6d2cbf579e2a93e483fb07b
Dmitry Simonov <dsimonov0291@gmail.com>
2021-05-01 21:10:28 +0700

    change font

--- config.def.h
+++ config.def.h
@@ -14,8 +14,8 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "FiraCode Nerd Font:style=Regular:size=10" };
-static const char dmenufont[]       = "FiraCode Nerd Font:style=Regular:size=10"; 
+static const char *fonts[]          = { "Iosevka Nerd Font:style=Regular:size=10" };
+static const char dmenufont[]       = "Iosevka Nerd Font:style=Regular:size=10"; 
 static const char col_gray1[]       = "#2e3440";
 static const char col_gray2[]       = "#3B4252";
 static const char col_gray3[]       = "#d8dee9";
@@ -68,7 +68,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "-e", "tmux", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 static const char *upvol[] = { "amixer", "-q", "sset", "Master", "1%+", NULL };
